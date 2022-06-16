e46fa90e444b925f5d181efa47e8e577175f4812
zz <zzhangz@foxmail.com>
2022-06-16 21:56:39 +0800

    [其他]更新README，优化打包脚本

---
+++ README.md
@@ -0,0 +1,17 @@
+![QuickLook icon](https://user-images.githubusercontent.com/1687847/29485863-8cd61b7c-84e2-11e7-97d5-eacc2ba10d28.png)
+
+# QuickLook.Plugin.HexViewer
+
+This plugin preview all file as Hex.
+
+![HexPreViewer](./images/QuickLook-Hex-PreViewer.png)
+
+## Development
+
+This plugin use [HexViewer.Wpf](https://github.com/fjeremic/HexView.Wpf) to render any file in Hex.
+
+In this project, use a [fork](https://github.com/goodemouce/HexView.Wpf) of HexViewer.Wpf to support .net framework
+
+## License
+
+MIT License.
--- Scripts/pack-zip.ps1
+++ Scripts/pack-zip.ps1
@@ -1,5 +1,21 @@
-Remove-Item ..\QuickLook.Plugin.HelloWorld.qlplugin -ErrorAction SilentlyContinue
+$packet_dir = "..\packet\"
+$packet_name = "QuickLook.Plugin.HexViewer"
+$packet_zip = $packet_dir + $packet_name + ".zip"
+$packet_file = $packet_dir + $packet_name + ".qlplugin"
+
+# 删除之前的文件
+Remove-Item $packet_file -ErrorAction SilentlyContinue
 
 $files = Get-ChildItem -Path ..\bin\Release\ -Exclude *.pdb,*.xml
-Compress-Archive $files ..\QuickLook.Plugin.HelloWorld.zip
-Move-Item ..\QuickLook.Plugin.HelloWorld.zip ..\QuickLook.Plugin.HelloWorld.qlplugin
+
+$packet_dir_exist = (Test-Path $packet_dir)
+
+# 创建输出文件夹
+if (-not $packet_dir_exist)
+{
+	$Null = New-Item -Path $packet_dir -ItemType "directory" 
+}
+
+# 打包
+Compress-Archive $files $packet_zip
+Move-Item $packet_zip $packet_file
