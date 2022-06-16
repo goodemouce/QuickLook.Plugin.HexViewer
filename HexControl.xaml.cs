4867c76384d05e1321ef88c2fff5edf00518103c
zz <zzhangz@foxmail.com>
2022-06-16 21:02:14 +0800

    [功能]创建项目

---
+++ HexControl.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace QuickLook.Plugin.HexViewer
+{
+    /// <summary>
+    /// HexControl.xaml 的交互逻辑
+    /// </summary>
+    public partial class HexControl : UserControl, INotifyPropertyChanged
+    {
+        private BinaryReader binaryReader;
+
+        public HexControl()
+        {
+            InitializeComponent();
+        }
+
+        public HexControl(String file): this()
+        {
+            OpenFile(file);
+        }
+
+        public void OpenFile(String file)
+        {
+            Reader = new BinaryReader(File.Open(file, FileMode.Open, FileAccess.Read, FileShare.Read));
+        }
+
+        public BinaryReader Reader
+        {
+            get => binaryReader;
+
+            set
+            {
+                if (binaryReader != null)
+                {
+                    binaryReader.Close();
+                }
+
+                binaryReader = value;
+                OnPropertyChanged();
+            }
+        }
+
+        /// <inheritdoc/>
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,41 @@
+﻿using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using QuickLook.Common.Plugin;
+
+namespace QuickLook.Plugin.HexViewer
+{
+    public class Plugin : IViewer
+    {
+        public int Priority => -1000;
+
+        public void Init()
+        {
+        }
+
+        public bool CanHandle(string path)
+        {
+            FileInfo fi = new FileInfo(path);
+            return (fi.Attributes & FileAttributes.Directory) == 0;
+        }
+
+        public void Prepare(string path, ContextObject context)
+        {
+            context.PreferredSize = new Size(635, 450);
+        }
+
+        public void View(string path, ContextObject context)
+        {
+            var viewer = new HexControl(path);
+
+            context.ViewerContent = viewer;
+            context.Title = $"{Path.GetFileName(path)}";
+
+            context.IsBusy = false;
+        }
+
+        public void Cleanup()
+        {
+        }
+    }
+}
